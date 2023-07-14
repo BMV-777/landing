9d447df4537b08ab8d2b7ec828ccc9c1af9e9cca
Maks Borenkov <borenkovmv@gmail.com>
2023-07-14 16:44:22 +0300

    block_menu

--- src/components/DishMenu/DishMenu.tsx
+++ src/components/DishMenu/DishMenu.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import css from "./DishMenu.module.css";
+
 import Image1 from "../../img/cards1.png";
 import Image2 from "../../img/cards2.png";
 import Image3 from "../../img/cards3.png";
@@ -7,22 +9,25 @@ import Image4 from "../../img/cards4.png";
 
 const DishMenu = () => {
   return (
-    <div>
+    <div className={css.block}>
       <div>
-        <p>
-          Latest <span>Offers</span>
+        <p className={css.title}>
+          Latest <span className={css.title_color}>Offers</span>
         </p>
       </div>
-      <div>
-        <div>
+      <div className={css.container_dishMenu}>
+        <div className={css.list_block}>
           <img src={Image1} alt="pizza" />
-          <p>Pepperoni Pizza</p>
-          <p>
-            $19.99<span>$24.9</span>
-          </p>
+          <div className={css.list_quantity}>
+            <p>Pepperoni Pizza</p>
+            <p>
+              <span>$19.99</span>
+              <span>$24.9</span>
+            </p>
+          </div>
           <button>Quick Shop</button>
         </div>
-        <div>
+        <div className={css.list_block}>
           <img src={Image2} alt="pizza" />
           <p>Pepperoni Pizza</p>
           <p>
@@ -30,7 +35,7 @@ const DishMenu = () => {
           </p>
           <button>Quick Shop</button>
         </div>
-        <div>
+        <div className={css.list_block}>
           <img src={Image3} alt="pizza" />
           <p>Pepperoni Pizza</p>
           <p>
@@ -38,7 +43,8 @@ const DishMenu = () => {
           </p>
           <button>Quick Shop</button>
         </div>
-        <div>
+        <div></div>
+        <div className={css.list_block}>
           <img src={Image4} alt="pizza" />
           <p>Pepperoni Pizza</p>
           <p>
